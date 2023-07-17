1d865dbb4c154ff6cf5b785cf218a8a78a1edc84
Sanjeewa Yapa <aquakloud0829@gmail.com>
2023-07-17 16:36:32 +0530

    "create util package"

--- src/main/java/com/aquakloud/JWTbackend/config/JwtRequestFilter.java
+++ src/main/java/com/aquakloud/JWTbackend/config/JwtRequestFilter.java
@@ -1,5 +1,13 @@
 package com.aquakloud.JWTbackend.config;
 
+import com.aquakloud.JWTbackend.service.JwtService;
+import com.aquakloud.JWTbackend.util.JwtUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,12 +20,45 @@ import java.io.IOException;
 @Component
 public abstract class JwtRequestFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private JwtService jwtService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
-        // Implement your JWT request filtering logic here
 
-        // Call the next filter in the chain
-        chain.doFilter(request, response);
+        final String requestTokenHeader = request.getHeader("Authorization");
+
+        String username = null;
+        String jwtToken = null;
+
+        if(requestTokenHeader !=null && requestTokenHeader.startsWith("Bearer ")){
+            jwtToken = requestTokenHeader.substring(7);
+            try {
+                username = jwtUtil.getUserNameFromToken(jwtToken);
+            }catch (IllegalArgumentException e){
+                System.out.println("Unable to get JWT Token");
+            }catch (ExpiredJwtException e){
+                System.out.println("JWT Token has expired");
+            }
+        } else {
+            System.out.println("JWT token does not start with Bearer");
+        }
+
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+                  UserDetails userDetails = jwtService.loadUserByUsername(username);
+
+               if (jwtUtil.validateToken(jwtToken, userDetails)) {
+
+                  UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                  usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                  SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+              }
+          }
+          chain.doFilter(request, response);
     }
 
 }
